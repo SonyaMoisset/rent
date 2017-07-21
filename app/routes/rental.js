fc1341fa014100d31706a3afc322c5f538a34fdd
smoisset <smoisset@worldremit.com>
2017-07-21 09:55:31 +0100

    Upload routes

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -27,7 +27,17 @@ export default Ember.Route.extend({
     },
 
     destroyRental(rental) {
-      rental.destroyRecord();
+      var review_deletions = rental.get('reviews').map(function (review) {
+        return review.destroyRecord();
+      });
+      Ember.RSVP.all(review_deletions).then(function() {
+        return rental.destroyRecord();
+      });
+      this.transitionTo('index');
+    },
+
+    destroyReview(review) {
+      review.destroyRecord();
       this.transitionTo('index');
     }
   }
